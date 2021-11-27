136434d82aa94fc80ba42e1d213fa1b7ecb800ec
Toya1001 <84680956+Toya1001@users.noreply.github.com>
2021-11-26 23:02:36 -0500

    updates to items module

--- resources/views/livewire/quik-alerts.blade.php
+++ resources/views/livewire/quik-alerts.blade.php
@@ -72,8 +72,6 @@
            <img src="{{ $image->temporaryUrl() }}" width="350px">
            @endif
            </div>
-
-
             <div class="flex justify-end mt-6">
                 <button class="px-6 py-2 leading-5 text-white transition-colors duration-200 transform bg-gray-700 rounded-md hover:bg-gray-600 focus:outline-none focus:bg-gray-600">Save</button>
             </div>
